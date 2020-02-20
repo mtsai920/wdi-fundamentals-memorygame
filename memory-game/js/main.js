4eb11dcc7007700c348708f1b80d488a9cab89ea
Megan <megantsai920@gmail.com>
2020-02-20 17:40:20 -0500

    created more game logic (broken?)

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,9 +1,20 @@
-console.log("Up and running!");
+const cards = ["queen", "queen", "king", "king"];
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+const cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+let cardOne = cards[2];
+let cardTwo = cards[0];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log(cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+	cardsInPlay[0] === cardsInPlay[1];
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
+
+console.log("User flipped " + cardTwo);
